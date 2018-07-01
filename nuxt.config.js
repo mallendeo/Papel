ca18909a051fe9da6b06057deb5602884dbdac6c
Mauricio Allende <mauricio.allende.o@gmail.com>
2018-07-01 14:24:24 -0400

    Add nuxtjs/pwa config

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,19 +4,47 @@ module.exports = {
     contractDev: process.env.CONTRACT_DEV || 'n1tVbiJotXAa64LN3it23YooZqBh82jqUXu',
     contractProd: process.env.CONTRACT_PROD || 'n1hy1FLTrw6uEf22Cbm4fBQKxVdHNipnGSa'
   },
+
+  modules: [
+    '@nuxtjs/pwa',
+  ],
+
+  workbox: {
+    runtimeCaching: [
+      { urlPattern: 'https://papel.app/.*' }
+    ]
+  },
+
+  manifest: {
+    name: 'Papel Code Playground',
+    short_name: 'Papel',
+    start_url: '.',
+    icons: [
+      {
+        src: '/icons/android-chrome-192x192.png',
+        sizes: '192x192',
+        type: 'image/png'
+      },
+      {
+        src: '/icons/android-chrome-512x512.png',
+        sizes: '512x512',
+        type: 'image/png'
+      }
+    ],
+    theme_color: '#292D3D',
+    background_color: '#292D3D',
+    display: 'standalone'
+  },
+  meta: {
+    charset: 'utf-8',
+    description: 'Code Playground on the Blockchain'
+  },
   head: {
     title: 'Code Playground on the Blockchain - Papel',
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: 'Code Playground on the Blockchain' }
-    ],
     link: [
       { rel: 'apple-touch-icon', sizes: '180x180', href: '/icons/apple-touch-icon.png' },
       { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/icons/favicon-32x32.png' },
       { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/icons/favicon-16x16.png' },
-      { rel: 'shortcut icon', type: 'image/x-icon', href: '/icons/favicon.ico' },
-      { rel: 'manifest', href: '/site.webmanifest' },
       { rel: 'mask-icon', href: '/icons/safari-pinned-tab.svg', color: '#6e87f7' },
       { rel: 'stylesheet', href: 'https://fonts.googleapis.com/icon?family=Material+Icons|Comfortaa|Source+Code+Pro|Inconsolata' },
       { rel: 'stylesheet', href: 'https://unpkg.com/firacode@1.205.0/distr/fira_code.css' }
