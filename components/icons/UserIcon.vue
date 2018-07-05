69c80f333b55c56e84cf3e57792671d7de485ed2
Mauricio Allende <mauricio.allende.o@gmail.com>
2018-07-05 14:51:20 -0400

    Custom icon colors

---
+++ components/icons/UserIcon.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg viewBox="0 0 40 45" xmlns="http://www.w3.org/2000/svg">
+    <path d="M20 23.2c6.2 0 11.1-5 11.1-11.2 0-6.3-5-11.3-11-11.3C13.8.7 9 5.7 9 12c0 6.2 5 11.2 11 11.2zm-6.8 2h-1.5C7 25.2 3.4 27.9.8 33.4 5.9 40.7 12.3 44.3 20 44.3c7.7 0 14-3.6 19.2-11a13 13 0 0 0-10.5-8.1H27a11.6 11.6 0 0 1-13.7 0z" stroke="#FFF" stroke-width="1.4" fill="none" fill-rule="evenodd" />
+  </svg>
+</template>
+
+<style scoped>
+svg path {
+  stroke: var(--color-text, white);
+}
+</style>
