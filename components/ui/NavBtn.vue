b5aa995da05e143dfa52ee4a6d7f8de1a39e61bb
Mauricio Allende <mauricio.allende.o@gmail.com>
2018-07-11 19:31:51 -0400

    Add nav-btn component

---
+++ components/ui/NavBtn.vue
@@ -0,0 +1,24 @@
+<template>
+  <button v-on="$listeners" class="btn row row--ai-center nav-btn">
+    <slot />
+  </button>
+</template>
+
+<style lang="scss" scoped>
+.nav-btn {
+  padding: 0 1rem;
+  height: 3rem;
+  border-radius: .25rem;
+  background: var(--color);
+  color: var(--color-light);
+
+  &:hover {
+    background: var(--color-accent);
+  }
+
+  &:disabled {
+    pointer-events: none;
+    opacity: .25;
+  }
+}
+</style>
