45b0828a804adf733058c71b1b58d5a53a99c7fb
Mauricio Allende <mauricio.allende.o@gmail.com>
2018-07-24 15:17:13 -0400

    Add loading indicator to action button

--- components/ui/ActionBtn.vue
+++ components/ui/ActionBtn.vue
@@ -1,8 +1,23 @@
 <template>
-  <button class="action-btn">
+  <button class="action-btn" :class="{ 'action-btn--loading': loading }">
+    <loading-indicator v-if="showLoading"/>
     <slot />
   </button>
 </template>
+
+<script>
+import LoadingIndicator from './LoadingIndicator.vue'
+
+export default {
+  components: {
+    LoadingIndicator
+  },
+  props: {
+    loading: { type: Boolean, default: false },
+    showLoading: { type: Boolean, default: false }
+  }
+}
+</script>
 
 <style lang="scss" scoped>
 .action-btn {
@@ -12,7 +27,6 @@
   position: relative;
 
   padding: .75rem 1.5rem;
-  align-self: flex-start;
   margin-top: 2rem;
 
   background: var(--color-accent);
@@ -21,8 +35,25 @@
   font-size: .8rem;
   border-radius: .25rem;
 
-  > *:first-child {
+  outline: none;
+
+  &--loading {
+    div.loading {
+      margin-right: 1rem;
+      opacity: 1;
+    }
+  }
+
+  > *:first-child:not(.loading) {
     margin-right: 1rem;
   }
+}
+
+div.loading {
+  margin-right: -1rem;
+  width: 1rem; height: 1rem;
+  opacity: 0;
+  transition: all .2s ease;
+  --color: white;
 }
 </style>
