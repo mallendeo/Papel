54890330c2297239f511162a7eded4602873cb19
Mauricio Allende <mauricio.allende.o@gmail.com>
2019-03-20 21:12:30 -0300

    add lowdb and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,12 +10,11 @@ module.exports = {
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
+    'plugin:vue/essential',
+    'prettier'
   ],
   // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
+  plugins: ['vue', 'import', 'prettier', 'standard'],
   // add your custom rules here
   rules: {}
 }
